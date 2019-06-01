80cb6ea77dfe29c0e971287318bdc41f2b605f2a
Amir Kamankesh <A.KamanKesh@persianswitch.net>
2019-06-01 18:13:49 +0430

    log geo location navigator

--- src/components/TestGeoLocation.jsx
+++ src/components/TestGeoLocation.jsx
@@ -4,6 +4,7 @@ class TestGeoLocation extends React.Component {
     componentDidMount() {
         if ('geolocation' in navigator) {
             alert('the geo location is enable');
+            console.log(navigator.geolocation);
             navigator.geolocation.getCurrentPosition((location) => {
                 console.log(location);
                 alert('your lat is ' + location.coords.latitude.toString() + ' and your lon is ' + location.coords.longitude.toString());
