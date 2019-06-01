bd0c51adca9414dc2eb057962698010eb643b047
Amir Kamankesh <A.KamanKesh@persianswitch.net>
2019-06-01 17:43:58 +0430

    add geo location support

---
+++ src/components/TestGeoLocation.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class TestGeoLocation extends React.Component {
+    componentDidMount() {
+        if ('geolocation' in navigator) {
+            // alert('the geo location is enable');
+            navigator.geolocation.getCurrentPosition((location) => {
+                console.log(location);
+                alert('your lat is ' + location.coords.latitude.toString() + ' and your lon is ' + location.coords.longitude.toString());
+            });
+        } else {
+            alert('the geo location api is disable !!');
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                test geo location component
+            </div>
+        );
+    }
+}
+
+export default TestGeoLocation;
