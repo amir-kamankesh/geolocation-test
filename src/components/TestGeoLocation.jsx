f538caa6cbee1fdaf76f70ba6b95eec5c771b832
Amir Kamankesh <A.KamanKesh@persianswitch.net>
2019-06-10 10:53:06 +0430

    geo location remove all alerts

--- src/components/TestGeoLocation.jsx
+++ src/components/TestGeoLocation.jsx
@@ -5,17 +5,14 @@ class TestGeoLocation extends React.Component {
         console.log('hello from amir');
         if ('geolocation' in navigator) {
             console.log('geolocation is enable');
-            alert('the geo location is enable');
-            // console.log(navigator.geolocation);
             navigator.geolocation.getCurrentPosition(function(location) {
                 console.log('your lat is ' + location.coords.latitude + ' and your lon is ' + location.coords.longitude);
-                alert('your lat is ' + location.coords.latitude + ' and your lon is ' + location.coords.longitude);
             }, function (error) {
                 console.log('hey we have error to read location from your device');
                 console.log(error);
             });
         } else {
-            alert('the geo location api is disable !!!');
+            console.log('the geo location api is disable !!!');
         }
     }
 
