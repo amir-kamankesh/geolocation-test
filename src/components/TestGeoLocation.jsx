181c21368d3aa81d7fac56bcaf57fac191072e01
Amir Kamankesh <A.KamanKesh@persianswitch.net>
2019-06-10 11:01:06 +0430

    try cache added

--- src/components/TestGeoLocation.jsx
+++ src/components/TestGeoLocation.jsx
@@ -6,7 +6,12 @@ class TestGeoLocation extends React.Component {
         if ('geolocation' in navigator) {
             console.log('geolocation is enable');
             navigator.geolocation.getCurrentPosition(function(location) {
-                console.log('your lat is ' + location.coords.latitude + ' and your lon is ' + location.coords.longitude);
+                console.log('in get current position');]
+                try {
+                    console.log('your lat is ' + location.coords.latitude + ' and your lon is ' + location.coords.longitude);
+                } catch (e) {
+                    console.log('we have error to access location object');
+                }
             }, function (error) {
                 console.log('hey we have error to read location from your device');
                 console.log(error);
