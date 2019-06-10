d2895dd397336b90cf9101754bd109494897e42b
Amir Kamankesh <A.KamanKesh@persianswitch.net>
2019-06-10 10:19:02 +0430

    add console logs to all if statements

--- src/components/TestGeoLocation.jsx
+++ src/components/TestGeoLocation.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 class TestGeoLocation extends React.Component {
     componentDidMount() {
+        console.log('hello from amir');
         if ('geolocation' in navigator) {
+            console.log('geolocation is enable');
             alert('the geo location is enable');
             console.log(navigator.geolocation);
             navigator.geolocation.getCurrentPosition(function(location) {
-                console.log(location);
+                console.log('your lat is ' + location.coords.latitude + ' and your lon is ' + location.coords.longitude);
                 alert('your lat is ' + location.coords.latitude + ' and your lon is ' + location.coords.longitude);
             }, function (error) {
+                console.log('hey we have error to read location from your device');
                 console.log(error);
             });
         } else {
